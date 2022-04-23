9231a87c5ce6b155eb9a2f235de2d9136d6b04a2
zhaolaiwang <tcoole@163.com>
2022-04-23 15:48:55 +0800

    update

--- rsa/rsa.hpp
+++ rsa/rsa.hpp
@@ -4,67 +4,72 @@ using namespace std;
 
 void getEulerValue(mpz_t &p, mpz_t &q, mpz_t &euler)
 {
-    mpz_t p_tmp, q_tmp, tmp;
+	mpz_t p_tmp, q_tmp, tmp;
 
-    mpz_init(p_tmp);
-    mpz_init(q_tmp);
-    mpz_init(tmp);
+	mpz_init(p_tmp);
+	mpz_init(q_tmp);
+	mpz_init(tmp);
 
-    mpz_init_set_ui(tmp, 1);
+	mpz_init_set_ui(tmp, 1);
 
-    mpz_sub(p_tmp, p, tmp);
-    mpz_sub(q_tmp, q, tmp);
+	mpz_sub(p_tmp, p, tmp);
+	mpz_sub(q_tmp, q, tmp);
 
-    mpz_mul(euler, p_tmp, q_tmp);
+	mpz_mul(euler, p_tmp, q_tmp);
 
-    mpz_clear(p_tmp);
-    mpz_clear(q_tmp);
-    mpz_clear(tmp);
+	mpz_clear(p_tmp);
+	mpz_clear(q_tmp);
+	mpz_clear(tmp);
 }
-void createPrimePQ(mpz_t &rand_p, mpz_t &rand_q,const int MAX_BITS){
-    unsigned long rand_seed = time(nullptr);
+void createPrimePQ(mpz_t &rand_p, mpz_t &rand_q, const int MAX_BITS)
+{
+	unsigned long rand_seed = time(nullptr);
 
-    gmp_randstate_t state;
-    gmp_randinit_default(state);
+	gmp_randstate_t state;
+	gmp_randinit_default(state);
 
-    gmp_randseed_ui(state,rand_seed);
+	gmp_randseed_ui(state, rand_seed);
 
-    mpz_urandomb(rand_p,state,MAX_BITS);
-    while(!mpz_probab_prime_p(rand_p,15)){
-        mpz_urandomb(rand_p,state,MAX_BITS);
-    };
-    mpz_nextprime(rand_q,rand_p);
-}
-void createPrimeE(mpz_t &e, mpz_t euler){
-    unsigned long rand_seed = time(nullptr);
-    mpz_t tmp,cons_one;
-
-    mpz_init_set_ui(tmp,2);// tmp != 1
-    mpz_init_set_ui(cons_one,1);
-    
-    gmp_randstate_t state;
-    gmp_randinit_default(state);
-
-    gmp_randseed_ui(state,rand_seed);
-
-    while(mpz_cmp(tmp,cons_one)){
-        mpz_urandomm(e,state,euler);
-        mpz_gcd(tmp,e,euler);
-    }
-    mpz_clear(tmp);
-    mpz_clear(cons_one);
-}
-void encrypt(mpz_t &ciphertext,mpz_t &plaintext,mpz_t &e,mpz_t &n){
-    mpz_t i,j,k,l;
-	mpz_init_set_ui(i,2);
-	mpz_init_set_ui(j,1);
-	mpz_init_set_ui(l,0);
-	mpz_init(k);
-    mpz_set(ciphertext,j);
-	mpz_mod(plaintext, plaintext, n);
-	while (mpz_cmp(e, l)>0) 
+	mpz_urandomb(rand_p, state, MAX_BITS);
+	while (!mpz_probab_prime_p(rand_p, 15))
 	{
-		if (mpz_odd_p(e)) 
+		mpz_urandomb(rand_p, state, MAX_BITS);
+	};
+	mpz_nextprime(rand_q, rand_p);
+}
+void createPrimeE(mpz_t &e, mpz_t euler)
+{
+	unsigned long rand_seed = time(nullptr);
+	mpz_t tmp, cons_one;
+
+	mpz_init_set_ui(tmp, 2); // tmp != 1
+	mpz_init_set_ui(cons_one, 1);
+
+	gmp_randstate_t state;
+	gmp_randinit_default(state);
+
+	gmp_randseed_ui(state, rand_seed);
+
+	while (mpz_cmp(tmp, cons_one))
+	{
+		mpz_urandomm(e, state, euler);
+		mpz_gcd(tmp, e, euler);
+	}
+	mpz_clear(tmp);
+	mpz_clear(cons_one);
+}
+void encrypt(mpz_t &ciphertext, mpz_t &plaintext, mpz_t &e, mpz_t &n)
+{
+	mpz_t i, j, k, l;
+	mpz_init_set_ui(i, 2);
+	mpz_init_set_ui(j, 1);
+	mpz_init_set_ui(l, 0);
+	mpz_init(k);
+	mpz_set(ciphertext, j);
+	mpz_mod(plaintext, plaintext, n);
+	while (mpz_cmp(e, l) > 0)
+	{
+		if (mpz_odd_p(e))
 		{
 			mpz_mul(ciphertext, ciphertext, plaintext);
 			mpz_mod(ciphertext, ciphertext, n);
@@ -78,17 +83,18 @@ void encrypt(mpz_t &ciphertext,mpz_t &plaintext,mpz_t &e,mpz_t &n){
 	mpz_clear(k);
 	mpz_clear(l);
 }
-void decrypt(mpz_t &plaintext,mpz_t &ciphertext,mpz_t &d,mpz_t &n){
-    mpz_t i,j,k,l;
-	mpz_init_set_ui(i,2);
-	mpz_init_set_ui(j,1);
-	mpz_init_set_ui(l,0);
+void decrypt(mpz_t &plaintext, mpz_t &ciphertext, mpz_t &d, mpz_t &n)
+{
+	mpz_t i, j, k, l;
+	mpz_init_set_ui(i, 2);
+	mpz_init_set_ui(j, 1);
+	mpz_init_set_ui(l, 0);
 	mpz_init(k);
-    mpz_set(plaintext,j);
+	mpz_set(plaintext, j);
 	mpz_mod(ciphertext, ciphertext, n);
-	while (mpz_cmp(d, l)>0) 
+	while (mpz_cmp(d, l) > 0)
 	{
-		if (mpz_odd_p(d)) 
+		if (mpz_odd_p(d))
 		{
 			mpz_mul(plaintext, plaintext, ciphertext);
 			mpz_mod(plaintext, plaintext, n);
@@ -102,8 +108,9 @@ void decrypt(mpz_t &plaintext,mpz_t &ciphertext,mpz_t &d,mpz_t &n){
 	mpz_clear(k);
 	mpz_clear(l);
 }
-void get_d(mpz_t &e,mpz_t &euler,mpz_t &tmp,mpz_t &a,mpz_t &b){
-    mpz_t cons_zero,cons_one,i;
+void get_d(mpz_t &e, mpz_t &euler, mpz_t &tmp, mpz_t &a, mpz_t &b)
+{
+	mpz_t cons_zero, cons_one, i;
 	mpz_init_set_ui(cons_zero, 0);
 	mpz_init_set_ui(cons_one, 1);
 	mpz_init(i);
@@ -115,7 +122,7 @@ void get_d(mpz_t &e,mpz_t &euler,mpz_t &tmp,mpz_t &a,mpz_t &b){
 		return;
 	}
 	mpz_mod(i, e, euler);
-	get_d(euler,i,tmp,b,a);
+	get_d(euler, i, tmp, b, a);
 	mpz_div(i, e, euler);
 	mpz_mul(i, i, a);
 	mpz_sub(b, b, i);
--- rsa_test.cpp
+++ rsa_test.cpp
@@ -1,12 +1,12 @@
-#include"rsa/rsa.hpp"
+#include "rsa/rsa.hpp"
 const int KEY_LEN = 128;
 int main()
 {
-    mpz_t p,q;
-    mpz_t n,e,d;
+    mpz_t p, q;
+    mpz_t n, e, d;
     mpz_t euler;
-    mpz_t msg_plain,msg_decrypt,msg_encrypt;
-    mpz_t tmp,y_tmp;
+    mpz_t msg_plain, msg_decrypt, msg_encrypt;
+    mpz_t tmp, y_tmp;
 
     mpz_init(p);
     mpz_init(q);
@@ -18,30 +18,30 @@ int main()
     mpz_init(euler);
     mpz_init(tmp);
     mpz_init(y_tmp);
-    mpz_init_set_ui(msg_plain,99999);
+    mpz_init_set_ui(msg_plain, 99999);
 
-    createPrimePQ(p,q,KEY_LEN);
-    gmp_printf("p,q=%Zd,%Zd\n",p,q);
+    createPrimePQ(p, q, KEY_LEN);
+    gmp_printf("p,q=%Zd,%Zd\n", p, q);
 
-    mpz_mul(n,p,q);
-    gmp_printf("n=p*q=%Zd\n",n);
+    mpz_mul(n, p, q);
+    gmp_printf("n=p*q=%Zd\n", n);
 
-    getEulerValue(p,q,euler);
-    gmp_printf("euler=%Zd\n",euler);
+    getEulerValue(p, q, euler);
+    gmp_printf("euler=%Zd\n", euler);
 
-    createPrimeE(e,euler);
-    gmp_printf("e=%Zd\n",e);
+    createPrimeE(e, euler);
+    gmp_printf("e=%Zd\n", e);
 
-    get_d(e,euler,tmp,d,y_tmp);
-    mpz_add(d,d,euler);
-    mpz_mod(d,d,euler);
-    gmp_printf("d=%Zd\n",d);
+    get_d(e, euler, tmp, d, y_tmp);
+    mpz_add(d, d, euler);
+    mpz_mod(d, d, euler);
+    gmp_printf("d=%Zd\n", d);
 
-    encrypt(msg_encrypt,msg_plain,e,n);
-    gmp_printf("msg_encrypt=%Zd\n",msg_encrypt);
+    encrypt(msg_encrypt, msg_plain, e, n);
+    gmp_printf("msg_encrypt=%Zd\n", msg_encrypt);
 
-    decrypt(msg_decrypt,msg_encrypt,d,n);
-    gmp_printf("msg_decrypt=%Zd\n",msg_decrypt);
-    
+    decrypt(msg_decrypt, msg_encrypt, d, n);
+    gmp_printf("msg_decrypt=%Zd\n", msg_decrypt);
+
     return 0;
 }
